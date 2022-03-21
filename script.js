ad1f23fa0dc47f2a28de92d50cc4061bfb07acdc
Coreypdumais <Coreypdumais@gmail.com>
2022-03-21 16:05:52 -0400

    added buttons for functions.

--- script.js
+++ script.js
@@ -1,37 +1,60 @@
-function press1() {
-    console.log("1 pressed")
-}
-function press2() {
-    console.log("2 pressed")
 
-}
-function press3() {
-    console.log("3 pressed")
+    function press1() {
+        console.log("1 pressed")
+    }
+    function press2() {
+        console.log("2 pressed")
+    
+    }
+    function press3() {
+        console.log("3 pressed")
+    
+    }
+    function press4() {
+        console.log("4 pressed")
+    }
+    function press5() {
+        console.log("5 pressed")
+    }
+    function press6() {
+        console.log("6 pressed")
+        
+    }
+    function press7() {
+        console.log("7 pressed")
+        
+    }
+    function press8() {
+        console.log("8 pressed")
+        
+    }
+    function press9() {
+        console.log("9 pressed")
+        
+    }
+    function press0() {
+        console.log("0 pressed")
+        
+    }
+    
+    function plus() {
+        console.log("plus")
+    }
+    
+    function minus() {
+        console.log("minus")
+    }
+    
+    function multiply() {
+        console.log("multiply")
+    }
+    
+    function divide() {
+        console.log("divide")
+    }
+    
+    document.getElementById("display").innerHTML = "NEW DATA HERE";
+    
 
-}
-function press4() {
-    console.log("4 pressed")
-}
-function press5() {
-    console.log("5 pressed")
-}
-function press6() {
-    console.log("6 pressed")
-    
-}
-function press7() {
-    console.log("7 pressed")
-    
-}
-function press8() {
-    console.log("8 pressed")
-    
-}
-function press9() {
-    console.log("9 pressed")
-    
-}
-function press0() {
-    console.log("0 pressed")
-    
-}
+
+
