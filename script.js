e129a1a74e39e4489945e76e7652beba34f4092a
Coreypdumais <Coreypdumais@gmail.com>
2022-03-21 13:04:37 -0400

    added buttons and functions for integers

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 function press1() {
-    output++;
+    console.log("1 pressed")
+}
+function press2() {
+    console.log("2 pressed")
+
+}
+function press3() {
+    console.log("3 pressed")
+
+}
+function press4() {
+    console.log("4 pressed")
+}
+function press5() {
+    console.log("5 pressed")
+}
+function press6() {
+    console.log("6 pressed")
+    
+}
+function press7() {
+    console.log("7 pressed")
+    
+}
+function press8() {
+    console.log("8 pressed")
+    
+}
+function press9() {
+    console.log("9 pressed")
+    
+}
+function press0() {
+    console.log("0 pressed")
+    
 }
